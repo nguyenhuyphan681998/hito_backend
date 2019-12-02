7a5614ca6ed1685e43a0cf7f0e36e1b7e874eaa3
Duong Le <tduongle10@gmail.com>
2019-12-02 22:03:13 +0700

    implement basic stuff

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def contact
+  end
+
+  def about
+  end
+
+  def login
+  end
+
+  def signup
+  end
+end
